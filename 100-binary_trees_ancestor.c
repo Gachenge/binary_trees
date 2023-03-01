f0a4b023727ab9066f10efd06f26f21a78e108e1
Gachenge <gachenge1@gmail.com>
2023-03-01 18:36:58 +0300

    ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,17 +7,18 @@
 * Return: their lowest common ancestor
 */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				const binary_tree_t *second)
 {
 	if (!first || !second)
 		return (0);
 	if (first == second)
 		return ((binary_tree_t *) first);
 
-	if (first == second->parent)
+	if (first == second->parent || !first->parent)
 		return (binary_trees_ancestor(first, second->parent));
 
-	if (second == first->parent)
+	if (second == first->parent || !second->parent)
 		return (binary_trees_ancestor(second, first->parent));
 
 	return (binary_trees_ancestor(first->parent, second->parent));
